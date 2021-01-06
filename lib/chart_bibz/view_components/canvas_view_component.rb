f6fefb8959aff6b9a79309ab87ea305573aea8c0
Thomas HUMMEL <thomas@hummel.link>
2021-01-06 12:36:59 +0100

    Add comments to the canvas view component

--- lib/chart_bibz/view_components/canvas_view_component.rb
+++ lib/chart_bibz/view_components/canvas_view_component.rb
@@ -8,22 +8,26 @@ module ChartBibz
       WIDTH = 400
       HEIGHT = 400
 
+      # @param args [Hash] The html options
+      # @return [void]
       def initialize(args = {})
         @args = args
       end
 
       # Generate the html canvas
+      # @return [String] The html canvas
       def render
         tag.canvas(**html_options)
       end
 
-      # Get the html id
+      # @return [String] The canvas html id
       def id
         html_options[:id]
       end
 
       private
 
+      # @return [String] The canvas html attributes
       def html_options
         @html_options ||= { id: "chart-#{Random.uuid}", width: WIDTH, height: HEIGHT }
           .merge(@args)
